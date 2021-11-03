cd0d45cd688b698c6f0a765280aadc8b14bbf8c1
ajearuth <ajearuth@student.42.fr>
2021-11-03 14:58:23 +0100

    quicksort b

--- srcs/quicksort.c
+++ srcs/quicksort.c
@@ -6,7 +6,7 @@
 /*   By: ajearuth <ajearuth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 19:18:15 by ajearuth          #+#    #+#             */
-/*   Updated: 2021/11/03 14:39:10 by ajearuth         ###   ########.fr       */
+/*   Updated: 2021/11/03 14:58:06 by ajearuth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	quick_sort_a(int *tab_a, int *tab_b, int len_a, int len_b)
 	while(len_a > i)
 	{
 		j = i + 1;
-		if(in_order == 0)
+		if(in_order(tab_a, len_a) == 0)
 			return (0);
 		else if (len_a == 2)
 		{
@@ -59,5 +59,38 @@ int	quick_sort_a(int *tab_a, int *tab_b, int len_a, int len_b)
 		}
 		++i;
 	}
-	return(quick_sort_a(tab_a, tab_b, len_a, len_b) && )
+	return (quick_sort_a(tab_a, tab_b, len_a, len_b) 
+		&& quick_sort_b(tab_a, tab_b, len_a, len_b));
+}
+
+int	quick_sort_b(int *tab_a, int *tab_b, int len_a, int len_b)
+{
+	int mediane;
+	int i;
+	int j;
+
+	i = 0;
+	mediane = find_mediane(tab_b, len_b);
+	while(len_b > i)
+	{
+		j = i + 1;
+		if(in_order(tab_b, len_b) == 0)
+			return (0);
+		else if (len_b == 2)
+		{
+			sb(tab_b);
+			return (0);
+		}
+		else
+		if (tab_b[i] >= mediane)
+				rb(tab_b, len_b);
+			else
+			{
+				pa(tab_a, tab_b, len_a, len_b);
+				ra(tab_a, len_a);
+				++len_b; 
+			}
+		}
+		++i;
+	return(0);
 }
